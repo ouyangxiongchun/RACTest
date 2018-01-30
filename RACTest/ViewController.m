2103ae707b3f801220a043ff68cc8218e2ed5815
ouyangxiongchun <ouyxc1145@2ncai.com>
2018-01-30 17:39:02 +0800

    dev

--- RACTest/ViewController.m
+++ RACTest/ViewController.m
@@ -17,7 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSLog(@"这是dev_master");
 }
 
 - (void)didReceiveMemoryWarning {
